bf943cbf4d0bc09a30e78bced7f056b13e013d3f
Matt Chan <matt.chan44@gmail.com>
2014-07-03 18:12:27 -0700

    added socket.io to server

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -31,4 +31,27 @@ angular.module('myNewProjectApp', [
         query: { method: 'GET', params: { taskController: 'active' }, isArray: true }
       }
     );
-  }]);
+  }])
+  .factory('socket', function ($rootScope) {
+    var socket = io.connect();
+    return {
+      on: function (eventName, callback) {
+        socket.on(eventName, function () {
+          var args = arguments;
+          $rootScope.$apply(function () {
+            callback.apply(socket, args);
+          });
+        });
+      },
+      emit: function (eventName, data, callback) {
+        socket.emit(eventName, data, function () {
+          var args = arguments;
+          $rootScope.$apply(function () {
+            if (callback) {
+              callback.apply(socket, args);
+            }
+          });
+        });
+      }
+    };
+  });
--- server.js
+++ server.js
@@ -38,7 +38,7 @@ var server = app.listen(config.port, config.ip, function () {
 
 //  Socketio setup
 var io = require('socket.io').listen(server);
-io.sockets.on('connection', function (socket) {
+io.on('connection', function (socket) {
   socket.emit('news', { hello: 'world' });
   socket.on('my other event', function (data) {
     console.log(data);
